900bda20f5a6ea596cea125dde6c2c6066eb1962
rafaelgcaldas01 <rafael.gcaldas01@gmail.com>
2020-12-17 23:54:39 -0300

    Project DoWhile - RocketSeat

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import '../styles/tailwind.css'
+import '../styles/globals.css'
+
+import { AnimateSharedLayout } from 'framer-motion'
+
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <AnimateSharedLayout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AnimateSharedLayout>
+  )
+}
+
+export default MyApp
